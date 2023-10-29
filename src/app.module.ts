66f1ec7f29962cf86e78c72d2242278eaffc59d9
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-10-29 19:57:22 +0300

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatalogModule } from './catalog/catalog.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { options } from './database/ormconfig';
 
 @Module({
-  imports: [CatalogModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatalogModule, TypeOrmModule.forRoot(options)],
 })
 export class AppModule {}
--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatalogService } from './catalog.service';
 import { CatalogController } from './catalog.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [CatalogController],
   providers: [CatalogService],
 })
