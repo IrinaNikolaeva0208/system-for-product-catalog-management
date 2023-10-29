3aad2218c9c1d77d064af0be61dc5ee6a521d266
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-10-29 20:25:03 +0300

    feat: complete catalog CRUD operations

--- src/catalog/catalog.controller.ts
+++ src/catalog/catalog.controller.ts
@@ -7,6 +7,7 @@ import {
   Body,
   Param,
   ParseUUIDPipe,
+  HttpCode,
 } from '@nestjs/common';
 import { CatalogService } from './catalog.service';
 import { ProductDto } from './dto/product.dto';
@@ -16,20 +17,31 @@ export class CatalogController {
   constructor(private readonly catalogService: CatalogService) {}
 
   @Get()
-  async getAllProducts() {}
+  async getAllProducts() {
+    return await this.catalogService.getAll();
+  }
 
   @Get(':id')
-  async getProductById(@Param('id', ParseUUIDPipe) id: string) {}
+  async getProductById(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.catalogService.getById(id);
+  }
 
   @Post()
-  async createProduct(@Body() productDto: ProductDto) {}
+  async createProduct(@Body() productDto: ProductDto) {
+    return await this.catalogService.create(productDto);
+  }
 
   @Patch(':id')
   async updateProduct(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() productDto: Partial<ProductDto>,
-  ) {}
+  ) {
+    return await this.catalogService.update(id, productDto);
+  }
 
+  @HttpCode(204)
   @Delete(':id')
-  async deleteProduct(@Param('id', ParseUUIDPipe) id: string) {}
+  async deleteProduct(@Param('id', ParseUUIDPipe) id: string) {
+    await this.catalogService.delete(id);
+  }
 }
--- src/catalog/catalog.service.ts
+++ src/catalog/catalog.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { ProductDto } from './dto/product.dto';
+import { NotFoundException } from '@nestjs/common/exceptions';
 
 @Injectable()
-export class CatalogService {}
+export class CatalogService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async getAll() {
+    return await this.productRepository.find();
+  }
+
+  async getById(id: string) {
+    const productToGet = await this.productRepository.findOneBy({ id });
+    if (!productToGet) throw new NotFoundException('Product not found');
+    return productToGet;
+  }
+
+  async create(product: ProductDto) {
+    const createdAlbum = this.productRepository.create(product);
+    return await this.productRepository.save(createdAlbum);
+  }
+
+  async update(id: string, updates: Partial<ProductDto>) {
+    const productToUpdate = await this.getById(id);
+    return await this.productRepository.save({ productToUpdate, ...updates });
+  }
+
+  async delete(id: string) {
+    const deleteAlbumResult = await this.productRepository.delete(id);
+    if (!deleteAlbumResult.affected)
+      throw new NotFoundException('Product not found');
+  }
+}
