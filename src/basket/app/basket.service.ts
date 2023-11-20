c75a43b258c39c792afd85fd1a3cfee4c8921ca4
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-11-20 21:52:33 +0300

    feat: integrate cqrs pattern into basket service

--- src/basket/app/basket.service.ts
+++ src/basket/app/basket.service.ts
@@ -1,63 +1,36 @@
-import { Injectable, NotFoundException, Inject } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Basket, Product } from 'src/utils/entities';
-import { RpcException } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { GetBasketQuery } from './queries';
+import {
+  CreateBasketCommand,
+  AddProductToBasketCommand,
+  RemoveProductFromBasketCommand,
+} from './commands';
 
 @Injectable()
 export class BasketService {
   constructor(
-    @InjectRepository(Basket)
-    private readonly basketRepository: Repository<Basket>,
-    @Inject('CATALOG_MICROSERVICE') private readonly catalogClient: ClientKafka,
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
   ) {}
 
   async addProduct(id: string, userId: string) {
     const basket = await this.getBasket(userId);
-
-    const newBasket = await new Promise<any>((resolve, reject) =>
-      this.catalogClient.send('product.byId', id).subscribe(
-        (product: Product) => {
-          if (!basket.products.find((pr) => pr.id == id))
-            basket.products.push(product);
-          resolve(basket);
-        },
-        (err) => reject(new RpcException(err.response)),
-      ),
-    );
-
-    return await this.basketRepository.save(newBasket);
+    return this.commandBus.execute(new AddProductToBasketCommand(id, basket));
   }
 
   async removeProduct(id: string, userId: string) {
     const basket = await this.getBasket(userId);
-    const productIndex = basket.products.findIndex((prod) => prod.id == id);
-    if (!(productIndex + 1))
-      throw new NotFoundException('No such product in basket');
-    basket.products.splice(productIndex, 1);
-    return await this.basketRepository.save(basket);
+    return this.commandBus.execute(
+      new RemoveProductFromBasketCommand(id, basket),
+    );
   }
 
   async getBasket(userId: string) {
-    const existingBasket = await this.basketRepository.findOne({
-      where: { user: { id: userId } },
-      relations: { products: true },
-    });
+    const basket = await this.queryBus.execute(new GetBasketQuery(userId));
+    if (basket) return basket;
 
-    if (existingBasket) return existingBasket;
-    const newBasket = this.basketRepository.create({
-      user: { id: userId },
-    });
-    const { id } = await this.basketRepository.save(newBasket);
-
-    return await this.basketRepository.findOne({
-      where: { id },
-      relations: { products: true },
-    });
-  }
-
-  onModuleInit() {
-    this.catalogClient.subscribeToResponseOf('product.byId');
+    await this.commandBus.execute(new CreateBasketCommand(userId));
+    return await this.queryBus.execute(new GetBasketQuery(userId));
   }
 }
