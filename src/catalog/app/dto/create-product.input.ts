1ab479e8ef427de1544b569b9cde87c5bb7a7a11
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-11-23 00:00:28 +0300

    feat: rewrite catalog service to schema-first approach

--- src/catalog/app/dto/create-product.input.ts
+++ src/catalog/app/dto/create-product.input.ts
@@ -1,24 +1,18 @@
-import { InputType, Field, Float } from '@nestjs/graphql/dist';
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 
-@InputType()
 export class CreateProductInput {
-  @Field()
   @IsNotEmpty()
   @IsString()
   name: string;
 
-  @Field()
   @IsNotEmpty()
   @IsString()
   description: string;
 
-  @Field(() => Float)
   @IsNotEmpty()
   @IsNumber()
   price: number;
 
-  @Field()
   @IsNotEmpty()
   @IsString()
   category: string;
--- src/utils/entities/product.entity.ts
+++ src/utils/entities/product.entity.ts
@@ -6,34 +6,25 @@ import {
   ManyToMany,
   OneToMany,
 } from 'typeorm';
-import { ObjectType, Field, ID, Float, Directive } from '@nestjs/graphql/dist';
 import { User, Basket, Order } from 'src/utils/entities';
 
-@ObjectType()
-@Directive('@key(fields: "id")')
 @Entity()
 export class Product {
-  @Field(() => ID)
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Field()
   @Column()
   name: string;
 
-  @Field()
   @Column({ nullable: true })
   description: string;
 
-  @Field(() => Float)
   @Column()
   price: number;
 
-  @Field()
   @Column()
   category: string;
 
-  @Field()
   @Column()
   ownerId: string;
 
